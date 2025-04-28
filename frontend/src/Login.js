2653b2e6e8cdd1339d35d2edb0312c14f32d6166
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-04-28 12:17:00 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -14,7 +14,7 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://3.227.19.133/login", formData);
+      const response = await axios.post("http://3.227.19.133:5000/login", formData);
       alert(response.data.message);
     } catch (error) {
       alert(error.response.data.message);
