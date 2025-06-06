c2d55eca6c6706c8f8ce90f16d4db7d30df367c5
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-06-06 11:42:46 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -16,6 +16,7 @@ function Login() {
     try {
       // const response = await axios.post("http://54.221.123.218:5000/login", formData);
       const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/login`, formData);
+      // HERE THE ENVIRONMENT VARIABLE SHOULD BE STARTED WITH REACT_APP ONLY
       // const response = await axios.post("http://backend-loadbalancer-ecs-1656454160.us-east-1.elb.amazonaws.com/login", formData);
       alert(response.data.message);
     } catch (error) {
