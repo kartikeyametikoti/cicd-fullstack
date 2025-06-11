bea35905b39ee3b74aa532d72e98fbbff6ceb9c8
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-06-11 12:15:24 +0530

    Update dev_env.groovy

--- vars/dev_env.groovy
+++ vars/dev_env.groovy
@@ -3,14 +3,14 @@ def call(){
   def envvars = [:]
   node {
   withCredentials([
-    string(credentialsId: 'image-tag', variable: 'image-tag'), 
+    string(credentialsId: 'image_tag', variable: 'image_tag'), 
     string(credentialsId: 'backend-image-uri', variable: 'backend-image-uri'), 
     string(credentialsId: 'frotnend-image-uri', variable: 'frotnend-image-uri'), 
     string(credentialsId: 'dev_ssh_ip', variable: 'dev_ssh_ip'),
     sshUserPrivateKey(credentialsId: 'd0e39f12-5b65-418a-8262-6a41e75e109e', keyFileVariable: 'ssh_key', usernameVariable: 'ssh_user')
   ]) {
   envvars = [
-    image_tag : image-tag,
+    image_tag : image_tag,
     backend_image_uri : backend-image-uri,
     frontend_image_uri : frontend-image-uri,
     ssh_ip : dev_ssh_ip,
