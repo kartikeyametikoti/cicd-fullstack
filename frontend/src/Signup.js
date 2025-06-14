dd54302d7ac60f27c66557045300a6f14b1e09bc
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-06-14 13:49:46 +0530

    Update Signup.js

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -10,7 +10,7 @@ function Signup() {
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };   
-     
+      
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
